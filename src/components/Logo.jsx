0cea03c6d87f4e2f2e18ed7bc35ffca96a34002e
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2023-06-27 10:09:47 -0300

    modificaciones en vista mobile, tamaño logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,10 +3,13 @@ import image from "../images/LogoJIS.png";
 
 function Logo() {
     return (
-      <div className="flex p-2 animate-pulse
-        mbl:justify-start 
-        pcs:flex-auto pcs:flex-row pcs:items-center pcs:justify-start">
-        <img src={image} alt="img-logo.png" className="w-32 h-32 ml-12" />  
+      <div className="flex  animate-pulse
+        mbl:justify-start mbl:p-0
+        pcs:p-2 pcs:flex-auto pcs:flex-row pcs:items-center pcs:justify-start">
+        <img src={image} alt="img-logo.png" className="
+        mbl:w-12 mbl:h-12 mbl:ml-6
+        sml:w-32 sml:h-32 sml:ml-12
+        " />  
         </div>
   )
 }
