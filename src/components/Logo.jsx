578967d10614f64905f50f95c04e56ec0e3044d0
Juan Ignacio Sayago <juanignaciosayago@gmail.com>
2025-02-24 10:23:40 -0300

    cambios en la organizacion de los proyectos

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import image from "../images/LogoJIS.png";
+import { useNavigate } from 'react-router-dom';
 
 function Logo() {
+  const navigate = useNavigate()
+  const handleClick = () => {
+     navigate('/')
+  } 
     return (
-      <div className="flex  animate-pulse
+      <button onClick={handleClick} className="flex  animate-pulse
         mbl:justify-start mbl:p-0
         pcs:p-2 pcs:flex-auto pcs:flex-row pcs:items-center pcs:justify-start">
         <img src={image} alt="img-logo.png" className="
         mbl:w-12 mbl:h-12 mbl:ml-6
         sml:w-32 sml:h-32 sml:ml-12
         " />  
-        </div>
+        </button>
   )
 }
 export default Logo;
--- src/pages/Proyectos.js
+++ src/pages/Proyectos.js
@@ -4,42 +4,53 @@ import Error404 from '../components/Error404';
 import PaginatedItems from '../components/Paginate';
 import data from "../data.json";
 
-
 export default function Proyectos() {
   const [proyectos, setProyectos] = useState([]);
+
   useEffect(() => {
-    setProyectos(data.proyectos);
+    // Ordenar los proyectos por fecha antes de setear el estado
+    const sortedProjects = data.proyectos.sort((a, b) => {
+      if (a.id === 5 ) {
+        a.fecha = "01/2000" 
+      }
+      if (b.id === 5) {
+        b.fecha = "01/2000" 
+      }
+      
+      const [monthA, yearA] = a.fecha.split("/").map(Number);
+      const [monthB, yearB] = b.fecha.split("/").map(Number);
+
+      return yearB - yearA || monthB - monthA; // Ordenar primero por año y luego por mes
+    });
+
+    setProyectos(sortedProjects);
   }, []);
-  
-  const [buscar, setBuscar] = useState([])
+
+  const [buscar, setBuscar] = useState("");
   let proyectList = proyectos;
 
   let mensaje = "No se encontraron proyectos con tecnología solicitada";
+  
   const handleChange = (e) => {
     e.preventDefault();
     setBuscar(e.target.value);
-}
+  };
 
   if (buscar.length !== 0) {
-   proyectList =  proyectos.filter(
-        p => p.tecnologias
-            .toString()
-            .toLowerCase()
-       .includes(buscar.toLowerCase()));
-    
-        mensaje = `No se encontraron proyectos con la tecnología "${buscar}"`;
+    proyectList = proyectos.filter(p => 
+      p.tecnologias.toString().toLowerCase().includes(buscar.toLowerCase())
+    );
+    mensaje = `No se encontraron proyectos con la tecnología "${buscar}"`;
   }
+
   return (
     <>
-      <Filter handleChange={ handleChange} />
-        {
-          proyectList.length > 0 
-                        ?
-          <PaginatedItems buscar={buscar } proyectList={proyectList} itemsPerPage={3}/>
-                        :
-                        <Error404 props={mensaje}/>
-        }
-      
+      <Filter handleChange={handleChange} />
+      {
+        proyectList.length > 0 
+          ? <PaginatedItems buscar={buscar} proyectList={proyectList} itemsPerPage={3} />
+          : <Error404 props={mensaje} />
+      }
     </>
-  )
+  );
 }
